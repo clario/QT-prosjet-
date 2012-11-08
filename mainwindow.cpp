89c4af30b3afdce8f723027db240c8a1376a967f
Martin Fjeldstad <martin.fjeldstad@gmail.com>
2012-11-08 09:13:30 +0100

    La inn en snutt for å demonstrere programattikalsk connection av pushButton(QPushbutton) og infoLabel(Qlabel)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,12 +11,15 @@ MainWindow::MainWindow(QWidget *parent) :
   //  QLabel * idiot = new QLabel("label nr 2",this);
     QPushButton *knapp = new QPushButton("knapp",this);
 
-
-
-
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked() {
+
+    ui->infoLabel->setText("TADAAA!");
+
 }
