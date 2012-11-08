bf654dd3f70af838bfd33ee50c17899769766049
Lars R. Lie <larsronny@gmail.com>
2012-11-08 17:08:27 +0100

    Cleanup main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
-//lalal
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -9,34 +9,6 @@ int main(int argc, char *argv[])
 
     return a.exec();
 }
-//test fra Lars
-//Hei på deg Richard x2
-//Hei Anders og Richard
-//HEI ANDERS ENDA ENG
-
-//Lagt inn frå iPad. OMG
-//Lagt inn frå OMG. iPad
-//Behold the mother of nedless gadgets are here!!
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//hallå balla
-
-
-
-
-
 
 
 
