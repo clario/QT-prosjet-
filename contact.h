323ebe868993892a4cadf8c279cb2032226c5bb6
Lars R. Lie <larsronny@gmail.com>
2012-11-08 19:06:55 +0100

    contact.h update

--- contact.h
+++ contact.h
@@ -1,10 +1,44 @@
 #ifndef CONTACT_H
 #define CONTACT_H
+#include <string>
+
+//Declaring calendar namespace
+namespace calendar{
+    class Contact;
+}
+
+
 
 class Contact
 {
 public:
     Contact();
+    Contact(std::string,int,std::string);
+    Contact(std::string,std::string,int,std::string);
+    ~Contact();
+
+    //getters
+    std::string getFname();
+    std::string getLname();
+    int getPhoneNumber();
+    std::string getEmail();
+    int getContId();
+
+    //Setters
+    std::string setFname(std::string);
+    std::string setLname(std::string);
+    int setPhoneNumber(int);
+    std::setEmail(std::string);
+
+
+
+
+private:
+    std::string fname;
+    std::string lname;
+    int phoneNumber;
+    std::string email;
+    int contId;
 };
 
 #endif // CONTACT_H
