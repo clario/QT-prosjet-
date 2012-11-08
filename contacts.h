fa2c8cd68890690e19a2fccf76441e44eef4f22e
Anders Tangedal Nordeide <antan_no@hotmail.com>
2012-11-08 18:50:53 +0100

    Oppdatert klassen med metoder/namespace

--- contacts.h
+++ contacts.h
@@ -2,15 +2,27 @@
 #define CONTACTS_H
 #include <contact.h>;
 
-//Bygger berreopp klasse
+//Bygger berre opp klassene med metoder. Venter med implementasjon til det er nødvendig
+//Tenkte det kunne være greit med eit førebels interface å jobbe etter ;)
+
+namespace calender {
+    class Contacts;
+}
 
 class Contacts
 {
 public:
     Contacts();
 
+    bool add(void);
+    bool remove(void);
+    int find(string); //søker etter match i fornavn og etternavn, returnerer plassering i tabellen
+    int find(int); //søker etter telefonnummer, returnerer plassering i tabellen
+
+    //Finn metodane eg har laga er muligens ikkje idelle. Er berre mockup
+
 private:
     contact array[]; //Antar at vi endrer datastruktur her seinare.
+    int size;
 };
-
 #endif // CONTACTS_H
