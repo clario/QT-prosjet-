b65788df80590625539ab49bc82778f395744615
Richard Haugen Olsen <einetunet@gmail.com>
2012-11-12 20:27:02 +0100

    EventView

--- eventview.cpp
+++ eventview.cpp
@@ -2,5 +2,77 @@
 
 EventView::EventView(QWidget *parent) :
     QWidget(parent)
+
+
 {
+
+    setWindowTitle("Avtaler");
+    //gridLayout = new QGridLayout(this);
+    eventTitle = QString("Brazillian Wax");
+    setViewMode();
+
+
 }
+
+
+void EventView::setViewMode(){
+    mainLayout = new QVBoxLayout(this);
+
+    eventTitleLabel = new QLabel(eventTitle);
+    eventTitleLabel->setFont(QFont("",25));
+    //From-stuff
+    fromLabel = new QLabel("Fra:");
+    fromLabel->setFont(QFont("",18));
+    fromDateEdit = new QDateEdit();
+    fromDateEdit->setDate(QDate(QDate::currentDate()));
+    fromTimeEdit = new QTimeEdit();
+    fromTimeEdit->setTime(QTime(QTime::currentTime()));
+    fromTimeLabel = new QLabel("Tid:");
+    fromDateLabel = new QLabel("Dato:");
+    //To-stuff
+    toLabel = new QLabel("Til:");
+    toLabel->setFont(QFont("",18));
+    toDateLabel = new QLabel("Dato:");
+    toDateEdit = new QDateEdit();
+    toDateEdit->setDate(QDate(QDate::currentDate()));
+    toTimeLabel = new QLabel("Tid:");
+    toTimeEdit = new QTimeEdit(QTime(QTime::currentTime()));
+
+
+    firstGridLayout = new QGridLayout();
+    firstGridLayout->addWidget(eventTitleLabel,0,0,3,1,Qt::AlignLeft);
+    firstGridLayout->addWidget(fromLabel,4,0);
+    firstGridLayout->addWidget(fromDateLabel,5,0);
+    firstGridLayout->addWidget(fromDateEdit,5,1);
+    firstGridLayout->addWidget(fromTimeLabel,5,2);
+    firstGridLayout->addWidget(fromTimeEdit,5,3);
+    firstGridLayout->addWidget(toLabel,6,0);
+    firstGridLayout->addWidget(toDateLabel,7,0);
+    firstGridLayout->addWidget(toDateEdit,7,1);
+    firstGridLayout->addWidget(toTimeLabel,7,2);
+    firstGridLayout->addWidget(toTimeEdit,7,3);
+
+    //Repeat
+
+    repeatCheckBox = new QCheckBox("Repeter");
+
+    mainLayout->addLayout(firstGridLayout);
+
+    firstHLayout = new QHBoxLayout();
+
+  //  mainLayout->addLayout(firstHLayout);
+
+
+
+   //all inside mainlayout
+
+}
+
+
+void EventView::setEditMode(){
+
+
+}
+
+
+
--- eventview.h
+++ eventview.h
@@ -1,18 +1,85 @@
 #ifndef EVENTVIEW_H
 #define EVENTVIEW_H
-
+#include <QLabel>
+#include <QTimeEdit>
+#include <QDateEdit>
+#include <QCheckBox>
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QRadioButton>
+#include <QComboBox>
+#include <QTextEdit>
+#include <QButtonGroup>
+#include <QPushButton>
 #include <QWidget>
+#include <QGroupBox>
+#include <QGridLayout>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QString>
 
 class EventView : public QWidget
 {
     Q_OBJECT
 public:
     explicit EventView(QWidget *parent = 0);
-    
+    void setViewMode();
+    void setEditMode();
+
+
+
 signals:
-    
+
 public slots:
-    
+
+private:
+
+    QString eventTitle;
+
+    QLabel *eventTitleLabel;
+    QPushButton *editModeToggle;
+    QCheckBox *allDayEvent;
+    QLabel *allDayEventLabel;
+    QLabel *fromLabel;
+    QLabel *fromDateLabel;
+    QDateEdit *fromDateEdit;
+    QLabel *fromTimeLabel;
+    QTimeEdit *fromTimeEdit;
+    QLabel *toLabel;
+    QLabel *toDateLabel;
+    QDateEdit *toDateEdit;
+    QLabel * toTimeLabel;
+    QTimeEdit *toTimeEdit;
+
+    QCheckBox *repeatCheckBox;
+    QComboBox * repeatComboBox;
+    QLabel * repeatLabel;
+    QLabel * miniEventTitleLable;
+    QLineEdit * eventTitleEdit;
+    QLabel * eventTypeLabel;
+    QGroupBox * eventTypeRadioBoxContainer;
+    QRadioButton * eventRadioButton;
+    QRadioButton * absenceRadioButton;
+    QLabel * typeLabel;
+    QComboBox *typeComboBox;
+    QLabel * eventDescriptionLabel;
+    QTextEdit * eventDescriptionTextBox;
+    QLabel * attendingLabel;
+    QTextEdit * textEditAttending;
+    QPushButton * addContact;
+    QGridLayout * gridLayout;
+    QPushButton * okPushButton;
+    QPushButton * cancelPushButton;
+    QPushButton * savePushButton;
+    QGridLayout * firstGridLayout;
+    QHBoxLayout * firstHLayout;
+    QHBoxLayout * secondHLayout;
+    QHBoxLayout * thirdHLayout;
+    QHBoxLayout * fourthHLayout;
+    QVBoxLayout * mainLayout;
 };
+
+
+
 
 #endif // EVENTVIEW_H
