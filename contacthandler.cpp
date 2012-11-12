9fca5683d6931459c8c84c63ccb78cb99717a859
Anders Tangedal Nordeide <antan_no@hotmail.com>
2012-11-12 17:28:47 +0100

    Lagt til ein del metoder i contactHandler.cpp

--- contacthandler.cpp
+++ contacthandler.cpp
@@ -9,4 +9,33 @@ ContactHandler::ContactHandler()
 {
 }
 
+bool ContactHandler::add(string fName, string lName, int phoneNumber, string email) {
+    Contact *tmp = Contact(idCounter, fName, lName, phoneNumber, email);
+    idCounter++;
+    container.push_back(tmp);
+    if (tmp==container.end()) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+bool ContactHandler::remove(string fName, string lName, int phoneNumber, string email) {
+    //Venter med denne til søkemetodene i contact er avklart
+}
+
+bool ContactHandler::remove(int cID) {
+    //Venter på ein metode for å fjekke ein kontakt mot ein anna kontakt sin cID
+}
+
+void ContactHandler::setCounter(int &nextID) {
+    idCounter=nextID;
+}
+
+void ContactHandler::setCounter(void) {
+    //Venter litt med denne
+}
+
+int ContactHandler::getSize(void) {
+    return container.size();
+}
