bab672e60fb68b506ee823a7ec26b922dd0d6091
Anders Tangedal Nordeide <antan_no@hotmail.com>
2012-11-12 16:27:10 +0100

    Oppdaterte contacthandler.h

--- contacthandler.h
+++ contacthandler.h
@@ -3,17 +3,11 @@
   * Klasseansvarlig: Lars
   */
 
-
-
 #ifndef CONTACTHANDLER_H
 #define CONTACTHANDLER_H
 #include <contact.h>;
-#include <string>;
-
-using namespace std;
-
-//Bygger berre opp klassene med metoder. Venter med implementasjon til det er nødvendig
-//Tenkte det kunne være greit med eit førebels interface å jobbe etter ;)
+#include <QString>;
+#include <QVector>;
 
 namespace calender {
     class ContactHandler;
@@ -25,15 +19,12 @@ public:
     ContactHandler();
     ~ContactHandler();
 
-    bool add(void);
+    bool add();
     bool remove(void);
-    int find(string); //søker etter match i fornavn og etternavn, returnerer plassering i tabellen
-    int find(int); //søker etter telefonnummer, returnerer plassering i tabellen
-
-    //Finn metodane eg har laga er muligens ikkje idelle. Er berre mockup
 
 private:
-    Contact array[]; //Antar at vi endrer datastruktur her seinare.
+    QVector container; //Antar at vi endrer datastruktur her seinare.
     int size;
+    int idCounter;
 };
 #endif // CONTACTHANDLER_H
