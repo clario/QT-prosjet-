b65dce3f58ac28fc847ce7cf4774db185f3ff9ac
Endre Oma <endre.88.oma@gmail.com>
2012-11-12 19:50:42 +0100

    La til en implementasjon av event og eventhandler

--- event.cpp
+++ event.cpp
@@ -5,6 +5,133 @@
 
 #include "event.h"
 
+using namespace calendar;
+
 Event::Event()
 {
+}
+
+QString Event::getTitle() const {
+    return title;
+}
+
+void Event::setTitle(const QString& _title) {
+    title = _title;
+}
+
+
+QDateTime Event::getStartDateTime() const {
+    return start;
+}
+
+bool Event::setStartDateTime(const QDateTime& startDateTime) {
+    start = startDateTime;
+    return true;
+}
+
+/**
+  * Returnerer en tidsstreng på formatet 2012-02-01T22:33:00
+  */
+QString Event::getStartAsString() const {
+    return "";
+}
+
+/**
+  * Denne er grei å bruke for å laste
+  * nn dato fra et serialisert objkt
+  * Krever formatet 2012-02-01T22:33:00
+  */
+bool Event::setStartAsString(const QString& start) {
+    return true;
+}
+
+QDate Event::getStartDate() const {
+    return start.date();
+}
+
+bool Event::setStartDate(const QDate& startDate) {
+    start.setDate(startDate);
+    return true;
+}
+
+QTime Event::getStartTime() const {
+    return start.time();
+}
+
+bool Event::setStartTime(const QTime& startTime) {
+    start.setTime(startTime);
+    return true;
+}
+
+/**
+  * Metoder for sluttdato
+  **/
+
+QDateTime Event::getEndDateTime() const {
+    return end;
+}
+
+bool Event::setEndDateTime(const QDateTime& endDateTime) {
+    end = endDateTime;
+}
+
+/**
+  * Returnerer en tidsstreng på formatet 2012-02-01T22:33:00
+  */
+QString Event::getEndAsString() const {
+    return "";
+}
+
+/**
+  * Denne er grei å bruke for å laste 
+  * nn dato fra et serialisert objkt
+  * Krever formatet 2012-02-01T22:33:00
+  */
+bool Event::setEndAsString(const QString& start) {
+    return true;
+}
+
+
+QDate Event::getEndDate() const {
+    return end.date();
+}
+
+bool Event::setEndDate(const QDate& endDate) {
+    end.setDate(endDate);
+    return true;
+}
+
+QTime Event::getEndTime() const {
+    return end.time();
+}
+
+bool Event::setEndTime(const QTime& endTime) {
+    end.setTime(endTime);
+    return true;
+}
+
+std::vector<QString> Event::getParticipants() const {
+    return std::vector<QString>();
+}
+
+void Event::setParticipants(std::vector<QString> _participants) {
+   participants = _participants;
+}
+
+bool Event::operator==(const Event& other) const {
+    bool equal = (title == other.getTitle());
+
+    if (equal) {
+        equal = (start == other.getStartDateTime());
+    }
+    
+    if (equal) {
+        equal = (end == other.getEndDateTime());
+    }
+
+    if (equal) {
+        equal = (participants == other.getParticipants());
+    }
+
+    return equal;
 }
--- event.h
+++ event.h
@@ -6,19 +6,88 @@
 #ifndef EVENT_H
 #define EVENT_H
 #include <string>
-#include <contact.h>
+#include <vector>
 
-using namespace std;
+#include <QDateTime>
+#include <QDate>
+#include <QTime>
+
+#include "contact.h"
+
+namespace calendar {
 
 class Event
 {
 public:
     Event();
+
+    QString getTitle() const;
+    void setTitle(const QString& title);
+
+    /** Metoder for startdato */
+
+    QDateTime getStartDateTime() const;
+    bool setStartDateTime(const QDateTime& startDateTime);
+
+    /**
+     * Returnerer en tidsstreng på formatet 2012-02-01T22:33:00
+     */
+    QString getStartAsString() const;
+
+    /**
+     * Denne er grei å bruke for å laste 
+     * nn dato fra et serialisert objkt
+     * Krever formatet 2012-02-01T22:33:00
+     */
+    bool setStartAsString(const QString& start);
+
+    QDate getStartDate() const;
+    bool setStartDate(const QDate& startDate);
+
+    QTime getStartTime() const;
+    bool setStartTime(const QTime& startTime);
+
+    /**
+     * Metoder for sluttdato
+     **/
+
+    QDateTime getEndDateTime() const;
+    bool setEndDateTime(const QDateTime& endDateTime);
+
+    /**
+     * Returnerer en tidsstreng på formatet 2012-02-01T22:33:00
+     */
+    QString getEndAsString() const;
+
+    /**
+     * Denne er grei å bruke for å laste 
+     * nn dato fra et serialisert objkt
+     * Krever formatet 2012-02-01T22:33:00
+     */
+    bool setEndAsString(const QString& start);
+
+
+    QDate getEndDate() const;
+    bool setEndDate(const QDate& endDate);
+
+    QTime getEndTime() const;
+    bool setEndTime(const QTime& endTime);
+
+    std::vector<QString> getParticipants() const;
+    void setParticipants(std::vector<QString> participants);
+
+    bool operator== (const Event& other) const;
+
 private:
-    string title;
-    string eventType;
-    string location;
-    calendar::Contact* participants;
+    QString title;
+    QString eventType;
+    QString location;
+
+    QDateTime start;
+    QDateTime end;
+
+    std::vector<QString> participants;
 };
 
+}; // End namespace calendar
 #endif // EVENT_H
