126ecebe08011bc356915bc4750a5e69e447e55b
Anders Tangedal Nordeide <antan_no@hotmail.com>
2012-11-12 15:44:23 +0100

    Tok vekk forferdelege compiler options fra .pro fila

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <QtGui/QApplication>
-#include "calenderview.h"
+#include <QtXml/QDomDocument>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-
-    CalenderView w;
-    w.show();
-
-    return a.exec();
+    QString name = "test";
+    QString fileName = "C:/Users/anders/github13-github.com-clario-QT-prosjet-/XML_struct.xml";
+    QDomDocument *doc = new QDomDocument(); //Definerer QDom treet
+    QFile file(fileName);
+    //qDebug() << (file.open(QIODevice::ReadOnly));
+    doc->setContent(&file);
+    file.close();
+    QDomElement root = doc->documentElement();
+    qDebug() << root.tagName();
+    return 0;
 }
-
 
 
 
