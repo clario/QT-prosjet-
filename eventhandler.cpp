525f7237f56feeff8a6aae8a5c5ffa70f499a4b9
Endre Oma <endre.88.oma@gmail.com>
2012-11-13 08:59:01 +0100

    Oppdaterte EventHandler::findEvents(from, to)

--- eventhandler.cpp
+++ eventhandler.cpp
@@ -16,12 +16,16 @@ EventHandler::~EventHandler() {
 }
 
 std::vector<Event> EventHandler::findEvents(const QDate& from, const QDate& to) const {
+	// Denne metoden finner alle events mellom to datoer. søket er som inklusivt.
+	// Det finner ALLE event som start, slutter eller pågpr innenfor et tisrom.
 	std::vector<Event> results;
 	std::vector<Event>::const_iterator it;
 
 	for (it = eventContainer.begin(); it != eventContainer.end(); it++) {
 		Event e = *it;
-		if (e.getStartDate() >= from && e.getStartDate() <= to) {
+		if ((e.getStartDate() >= from && e.getStartDate() <= to) ||
+		    (e.getEndDate() >= from && e.getEndDate() <= to) ||
+		    (e.getStartDate() <= from && e.getEndDate() >= to)) {
 			results.push_back(e);
 		}
 	}
