09c77db090c67a336dd1fae4550e2f233899fce4
Anders Tangedal Nordeide <antan_no@hotmail.com>
2012-11-17 14:02:53 +0100

    *

--- filewriter.cpp
+++ filewriter.cpp
@@ -1,5 +1,28 @@
 #include "filewriter.h"
+#include "contact.h"
+#include "event.h"
 
-FileWriter::FileWriter()
+FileWriter::FileWriter(QFile targetFile, auto endOfIterator, std::iterator itr, char inputTypeFlag)
 {
+    target=targetFile;
+    source=itr;
+    targetType=inputTypeFlag;
+}
+
+FileWriter::~FileWriter()
+{
+}
+
+bool FileWriter::save()
+{
+    if (targetType='c') {
+        while (c!=end) {
+            Contact c = (*source);
+
+        }
+    } else if(targetType='e') {
+
+    } else {
+        return false;
+    }
 }
--- filewriter.h
+++ filewriter.h
@@ -1,10 +1,31 @@
 #ifndef FILEWRITER_H
 #define FILEWRITER_H
+#include <QtCore/QFile>
+#include <iterator>
+#include <contact>
+#include <event>
 
 class FileWriter
 {
 public:
-    FileWriter();
+    /*@input QFile(targetFile), std::iterator from container, const static inputFlag to identify content.
+    */
+    FileWriter(QFile targetFile, auto endOfIterator, std::iterator itr, char inputTypeFlag);
+    ~FileWriter();
+
+    const static char typeIsContact='c';
+    const static char typeIsEvent='e';
+
+    /*@output Returns true if the current save was sucsessfull
+    */
+    bool save(void);
+
+private:
+    const QFile target;
+    const std::iterator source;
+    const char sourceType;
+    auto end;
+
 };
 
 #endif // FILEWRITER_H
