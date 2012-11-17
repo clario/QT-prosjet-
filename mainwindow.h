cc4f53275438141a9a24b362b08c99c46ce2cd62
Martin Fjeldstad <martin.fjeldstad@gmail.com>
2012-11-17 12:55:46 +0100

    Rettet typo

--- mainwindow.h
+++ mainwindow.h
@@ -18,7 +18,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 public:
     explicit MainWindow(QWidget *parent = 0);
-    MainWindow(Troller *troll, ContactHandler *chandler, EventHander *ehandler);
+    MainWindow(Troller *troll, ContactHandler *chandler, EventHandler *ehandler);
 
 private:
 
@@ -33,6 +33,7 @@ private:
 
     void createDock();
     void createCalendar();
+
 
 signals:
     
