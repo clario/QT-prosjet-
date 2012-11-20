46d656baaa1a279d713e770a180727c0f067c147
Martin Fjeldstad <martin.fjeldstad@gmail.com>
2012-11-20 07:04:22 +0100

    Lyset i enden av tunnellen.. så lenge det ikke er toget.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <QtGui/QApplication>
+#include "troller.h"
+#include "calendarview.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    return app.exec();
+
+    Troller calendar;
+    calendar.run();
+
+    //app.setActiveWindow(&calendar.mainWindow);
+
+    int retVal = app.exec();
+    calendar.hasClosed();
+
+    return retVal;
 }
 
 
